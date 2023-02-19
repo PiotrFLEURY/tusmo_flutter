1aee79162123b7f1b8fba2a0b67c87f49177257a
Piotr FLEURY <piotr.fleury@gmail.com>
2023-02-19 17:52:07 +0100

    set stylemostly like tusmo (#1)
    * set stylemostly like tusmo
    
    * remove submit lenght check for more fun

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tusmo_flutter/home/states/provider.dart';
+import 'package:tusmo_flutter/home/views/word.dart';
 
 class HomePage extends ConsumerWidget {
   HomePage({super.key});
@@ -25,24 +26,12 @@ class HomePage extends ConsumerWidget {
                   ),
             ),
             const Spacer(),
-            Text(
-              state.obfuscatedWord,
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                    color: Colors.white,
-                  ),
+            Word(
+              value: state.obfuscatedWord,
+              letterColor: Colors.green,
             ),
             const Spacer(),
-            ...state.submittedWords
-                .map(
-                  (word) => Text(
-                    word,
-                    style: Theme.of(context).textTheme.labelLarge!.copyWith(
-                          color: Colors.white,
-                        ),
-                  ),
-                )
-                .toList(),
+            ...state.submittedWords.map((value) => Word(value: value)).toList(),
             const Spacer(),
             SizedBox(
               width: MediaQuery.of(context).size.width * 0.5,
@@ -106,7 +95,6 @@ class HomePage extends ConsumerWidget {
                   style: Theme.of(context).textTheme.headlineLarge,
                 ),
                 const Spacer(),
-                // Text word was: ${state.wordToFind}
                 Text(
                   'Word was: ${ref.read(tusmoStateProvider).wordToFind}',
                   style: Theme.of(context).textTheme.labelLarge,
